b5fe6c25fb54a53deb0f9fd0eacdb6f9917a1ff9
justinscottjenecke <justinjenecke@gmail.com>
2024-07-23 09:31:29 +0200

    creates data models + installs dev dependancies for ef core

---
+++ MenuApp/Models/Dish.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MenuApp.Models;
+
+public class Dish
+{
+    public int Id {get; set;}
+
+    [Required]
+    public string Name {get; set;}
+
+    public String ImageUrl {get; set;}
+
+    public Double Price {get; set;}
+
+    public List<DishIngredient>? DishIngredients { get; set; }
+}
---
+++ MenuApp/Models/DishIngredient.cs
@@ -0,0 +1,9 @@
+﻿namespace MenuApp.Models;
+
+public class DishIngredient
+{
+    public int Id { get; set; }
+    public Dish Dish { get; set; }
+    public int IngredientId { get; set; }
+    public Ingredient Ingredient { get; set; }
+}
